ef90808c69888bd9bc0704a97ae77c4acb44473c
DaveG998 <117363083+DaveG998@users.noreply.github.com>
2023-12-25 11:07:51 +0100

    mostly finished
    missing round

--- script.js
+++ script.js
@@ -1,32 +1,114 @@
-const a = 0;
-const b = 0;
-const op = "+";
+let a = "";
+let b = "";
+let op = "";
 
-let display = 0;
+let display = "";
+
+let changed = false;
 
 const btnNumbers = document.querySelectorAll(".numbers");
+const screen = document.querySelector("#display");
+const btnOperators = document.querySelectorAll(".operator");
+const btnEquals = document.querySelector("#equals");
+const btnPlusMinus = document.querySelector("#plus-minus");
+const btnClear = document.querySelector("#clear");
+const btnRedo = document.querySelector("#redo");
+const btnDot = document.querySelector("#dot");
 
 btnNumbers.forEach((btn) => {
   btn.addEventListener("click", () => {
     const number = btn.textContent;
     display += number;
+    displayScreen(display);
+    changed = true;
   });
 });
 
-function add(a, b) {
-  return a + b;
+btnOperators.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (changed) {
+      if (a == "") {
+        op = btn.textContent;
+        a = display;
+      } else {
+        b = display;
+        calc();
+        op = btn.textContent;
+      }
+      display = "";
+      changed = false;
+    } else if (!changed) {
+      op = btn.textContent;
+    }
+  });
+});
+
+btnEquals.addEventListener("click", calc);
+
+btnPlusMinus.addEventListener("click", () => {
+  display = display.charAt(0) == "-" ? display.substring(1) : "-" + display;
+  displayScreen(display);
+});
+
+btnClear.addEventListener("click", () => {
+  a = "";
+  b = "";
+  op = "";
+  display = "";
+  displayScreen(display);
+});
+
+btnRedo.addEventListener("click", () => {
+  if (display.length > 1) {
+    display = display.substring(0, display.length - 1);
+  } else if (display.length == 1) {
+    display = "";
+    changed = false;
+  }
+  displayScreen(display);
+});
+
+btnDot.addEventListener("click", () => {
+  if (display.indexOf(".") == -1) display += ".";
+});
+
+function calc() {
+  if (changed && a != "") {
+    b = display;
+    operator(parseFloat(a), parseFloat(b), op);
+    b = "";
+    if (a == Infinity) {
+      displayScreen("ERROR");
+      a = "";
+      b = "";
+      op = "";
+      display = "";
+    } else displayScreen(a);
+    display = "";
+    changed = false;
+  }
 }
 
-function subtract(a, b) {
-  return a - b;
+function displayScreen(display) {
+  if (display == "") {
+    screen.textContent = "0";
+  } else screen.textContent = display;
 }
 
-function multiply(a, b) {
-  return a * b;
+function add(num1, num2) {
+  a = num1 + num2;
 }
 
-function divide(a, b) {
-  return a / b;
+function subtract(num1, num2) {
+  a = num1 - num2;
+}
+
+function multiply(num1, num2) {
+  a = num1 * num2;
+}
+
+function divide(num1, num2) {
+  a = num1 / num2;
 }
 
 function operator(a, b, op) {
